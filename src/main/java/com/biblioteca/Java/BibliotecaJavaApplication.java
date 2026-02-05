5d16c562c2bf6febd4660ed346d426b2a5ea30e9
Moroni <moronioliveira24@gmail.com>
2026-02-05 20:42:50 -0300

    Initial commit

---
+++ src/main/java/com/biblioteca/Java/BibliotecaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaJavaApplication.class, args);
+	}
+
+}
